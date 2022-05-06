910313ae0a9cde687f41f23fcd3c162acd8310d6
leke <akinsanyaleke@yahoo.com>
2022-05-06 14:05:40 -0400

    ok

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! Ok!!";
+		return "Greetings from Spring Boot + Tanzu! Ok!! Ok!!!";
 	}
 
 }
